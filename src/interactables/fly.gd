5e25fdd593c2629f7e95a19312e49d6a45e6d246
TheMintyMan <william.mintie@gmail.com>
2026-04-16 22:49:38 +1000

    general game update

--- src/interactables/fly.gd
+++ src/interactables/fly.gd
@@ -3,9 +3,9 @@ extends Food
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	super()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	super(delta)
